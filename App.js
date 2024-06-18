2c86396c743295457ff19a2dacb3bd0fdf726759
Raukul2810 <2810Raukul@gmail.com>
2024-06-18 15:13:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navigation from './Navigation';
+import Home from './Home';
+import Contact from './Contact';
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <br></br>
+      <Home/>
+      <br></br>
+      <Contact/>
+      </div>      
+  );
+}
+
+export default App;
